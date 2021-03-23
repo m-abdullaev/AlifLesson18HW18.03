8e85adc0bdfacfbc657c8627dd3e37c3b775bc59
Maruf Abdullaev <m.abdullaev4188@gmail.com>
2021-03-22 23:40:06 -0400

    Add project files.

---
+++ AlifLesson18HW18.03/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AlifLesson18HW18._03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ AlifLesson18HW18.03/Repos/RepositoryBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlifLesson18HW18._03.Repos
+{
+    class RepositoryBase
+    {
+
+    }
+}
